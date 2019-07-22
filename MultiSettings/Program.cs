4432051a156d586d7c9dab8c6807121110689489
William JIANG <williamjiang0218@gmail.com>
2019-07-22 21:03:41 +1000

    Add project files.

---
+++ MultiSettings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
